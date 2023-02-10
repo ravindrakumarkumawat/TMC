274b7ecde76844830508179943de2bb4f892b831
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2023-02-10 04:14:52 +0000

    Added the user login

--- client/src/Pages/Dashboard/index.js
+++ client/src/Pages/Dashboard/index.js
@@ -1,7 +1,10 @@
+import { useContext } from 'react'
+import { AuthContext } from '../../Context/Authentication/AuthProvider'
 const Dashboard = () => {
+    const { user } = useContext(AuthContext)
     return (
         <div>
-            Hey User, you are
+            Hey User, you are {user.username}
         </div>
     )
 }
