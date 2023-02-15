5d7e9c3ad5c716f3a2c661f82b42c909784972c4
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2023-02-15 11:11:54 +0000

    name chan

--- server/utils/generateHashPassword.js
+++ server/utils/generateHashPassword.js
@@ -2,6 +2,6 @@ const bcrypt = require('bcrypt')
 
 const generateHashPassword = async (password) => bcrypt.hash(password, await bcrypt.genSalt())
 
-const comparePassword = async (password, restaurantPassword) => bcrypt.compare(password, restaurantPassword)
+const comparePassword = async (password, userPassword) => bcrypt.compare(password, userPassword)
 
 module.exports = { generateHashPassword, comparePassword }
